5e8a2bf67907a4b1e207b91d355b28ece6fbb808
murphl3 <lmurphy3@luc.edu>
2024-09-07 17:33:28 -0500

    Completed getmove.lua

--- getmove.lua
+++ getmove.lua
@@ -1,11 +1,14 @@
 -- getmove.lua
 local function getmove()
+	io.write("Please enter your move: ")
 	return {
 		start = {
-			
+			x = io.read("*n"),
+			y = io.read("*n")
 		},
 		finish = {
-			
+			x = io.read("*n"),
+			y = io.read("*n")
 		}
 	}
 end
