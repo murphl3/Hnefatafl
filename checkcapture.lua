6492157fd549ac75758b186cf6105ab88758c632
murphl3 <lmurphy3@luc.edu>
2024-09-10 16:04:47 -0500

    Set getcapture to true for demonstration while working

--- checkcapture.lua
+++ checkcapture.lua
@@ -1,6 +1,22 @@
 -- checkcapture.lua
 local getneighbors = require("getneighbors")
 local function checkcapture(pos, moved, board, pieces, player)
+  if true then return true end
+  local neighbors = getneighbors(pos, board)
+  if board[pos.x][pos.y] == 4 then
+    if board.defenders == 0 then
+      local captured = true
+      for i=1,#neighbors,1 do
+        if (board[neighbors[i].x][neighbors[i].y] == 1) or (((neighbors.x == 0) or (neighbors.x == board.width)) and ((neighbors.y == 0) or (neighbors.y == board.height))) then
+          captured = false
+          break
+        end
+      end
+      return captured
+    else
+      
+    end
+  end
   return false
 end
 return checkcapture
